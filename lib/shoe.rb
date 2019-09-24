2ba18e951d322afc7ec46b6a66865a8217552d6a
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-24 21:50:20 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,40 +1,12 @@
-#class Shoe
-#  attr_accessor :color, :size, :material, :condition
-#  attr_reader :brand
-  
-#  BRANDS = []
-
-#  def initialize(brand)
-#    @brand = brand
-#    BRANDS << brand unless BRANDS.include? @brand
-#  end
-
-#  def cobble
-#    self.condition = "new"
-#    puts "Your shoe is as good as new!"
-#  end
-
-#end
-
-
 class Shoe
   attr_accessor :color, :size, :material, :condition
-
-  BRANDS = []
+  attr_reader :brand
   
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
-    BRANDS << brand.uniq
-    BRANDS.uniq    
- end
-  
-  def brand=(brand)
-    @brand = brand
-  
-  end
-  
-  def brand
-    @brand
+    BRANDS << brand unless BRANDS.include? @brand
   end
 
   def cobble
@@ -43,3 +15,5 @@ class Shoe
   end
 
 end
+
+
